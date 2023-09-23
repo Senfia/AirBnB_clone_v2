f196a6e0fc6a96c3b6c5fe25c5a49400adc571c9
root <root@DESKTOP-JKHB9QO>
2023-09-23 22:21:11 +0000

    print more variables

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module contains a script that starts a Flask web application.
+Adding more routes
+Usage: python3 -m 0-hello_route
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ displays Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def print_c(text):
+    """ Prints C followed by a text passed """
+    return "C %s" %text.replace("_", " ")
+
+
+@app.route('/python/<string:text>', strict_slashes=False)
+def print_python(text):
+    """ Prints Python followed by a text passed """
+    return "Python %s" %text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
