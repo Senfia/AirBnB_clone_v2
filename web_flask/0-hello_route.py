6bc617b3b23124ed141e0830299daa87298b6c89
root <root@DESKTOP-JKHB9QO>
2023-09-23 18:34:31 +0000

    working

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+This module contains a script that starts a Flask web application.
+Usage: python3 -m 0-hello_route
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """ displays Hello HBNB """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
